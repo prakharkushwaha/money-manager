7d23a761440e6940b0d86b5e66b7e691ed3c8fc0
prakharwork <prakharkushwaha2003@gmail.com>
2025-07-15 22:12:29 +0530

    deployment started firstly

---
+++ moneymanager/src/main/java/in/prakhar/moneymanager/controller/ExcelController.java
@@ -0,0 +1,36 @@
+package in.prakhar.moneymanager.controller;
+
+import in.prakhar.moneymanager.service.ExcelService;
+import in.prakhar.moneymanager.service.ExpenseService;
+import in.prakhar.moneymanager.service.IncomeService;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/excel")
+@RequiredArgsConstructor
+public class ExcelController {
+
+    private final ExcelService excelService;
+    private final IncomeService incomeService;
+    private final ExpenseService expenseService;
+
+    @GetMapping("/download/income")
+    public void downloadIncomeExcel(HttpServletResponse response) throws IOException {
+        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        response.setHeader("Content-Disposition", "attachment; filename=income.xlsx");
+        excelService.writeIncomesToExcel(response.getOutputStream(), incomeService.getCurrentMonthIncomesForCurrentUser());
+    }
+
+    @GetMapping("/download/expense")
+    public void downloadExpenseExcel(HttpServletResponse response) throws IOException {
+        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        response.setHeader("Content-Disposition", "attachment; filename=expense.xlsx");
+        excelService.writeExpensesToExcel(response.getOutputStream(), expenseService.getCurrentMonthExpensesForCurrentUser());
+    }
+}
