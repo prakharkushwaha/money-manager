9300d0f5316f2109ee06f53ddb237cf1b44b3656
prakharwork <prakharkushwaha2003@gmail.com>
2025-07-16 02:19:14 +0530

    updated security config

--- moneymanager/src/main/java/in/prakhar/moneymanager/config/SecurityConfig.java
+++ moneymanager/src/main/java/in/prakhar/moneymanager/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedOriginPatterns(List.of(frontendUrl));
         configuration.setAllowCredentials(true);
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization","Content-Type", "Accept"));
